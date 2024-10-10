2226cd2ff33484f59cbcad2b6a4976b6e5068843
MoQuant <112667516+MoQuant@users.noreply.github.com>
2024-10-10 15:21:48 -0400

    Add files via upload

--- stockNews.py
+++ stockNews.py
@@ -7,7 +7,7 @@ import json
 import numpy as np
 import pandas as pd
 from bs4 import BeautifulSoup
-from textblob import TextBlob 
+from textblob import TextBlob
 import time 
 
 # Display all rows
@@ -16,107 +16,86 @@ pd.set_option('display.max_rows', None)
 # Display all columns
 pd.set_option('display.max_columns', None)
 
-def CleanHTML(f):
-    def Solve(*a, **b):
-        data = f(*a, **b)
-        result = {}
-        for stock, article in data.items():
-            for story in article:
-                soup = BeautifulSoup(story, 'html.parser')
-                sentences = [p.get_text() for p in soup.find_all("p")][1:]
-            result[stock] = sentences
-        return result
-    return Solve
 
-def Extract(f):
-    def Solve(*a, **b):
-        stocks, tickers, news = f(*a, **b)
-        data = {tick:[] for tick in stocks}
-        for ticker, article in zip(tickers, news):
-            data[ticker].append(article)
+def GatherSentiment(lowerPOL, upperPOL, lowerSBJ, upperSBJ):
+    def HandleSentiment(f):
+        def SolveForIt(*a, **b):
+            data = f(*a, **b)
+            stocks = list(data.keys())
+            result = {stk:{'positive':0,'negative':0,'opinion':0,'fact':0} for stk in stocks}
+            for ticker, paragraph in data.items():
+                for sentence in paragraph:
+                    ML = TextBlob(sentence).sentiment
+                    POL = ML.polarity
+                    SBJ = ML.subjectivity
+                    if POL >= upperPOL:
+                        result[ticker]['positive'] += 1
+                    elif POL <= lowerPOL:
+                        result[ticker]['negative'] += 1
+                    else:
+                        pass
+                    if SBJ >= upperSBJ:
+                        result[ticker]['fact'] += 1
+                    elif SBJ <= lowerSBJ:
+                        result[ticker]['opinion'] += 1
+            return result
+        return SolveForIt
+    return HandleSentiment
+
+def ExtractFromHTML(f):
+    def ProblemSolution(*a, **b):
+        tickers, stocks, result = f(*a, **b)
+        data = {stk:[] for stk in stocks}
+        for stock, article in zip(stocks, result):
+            soup = BeautifulSoup(article, 'html.parser')
+            data[stock] += [u.get_text() for u in soup.find_all('p')]
         return data
+    return ProblemSolution
+
+def CreateDataFrame(f):
+    def Solve(*a, **b):
+        z = f(*a, **b)
+        final_result = []
+        stocks = list(z.keys())
+        for i in stocks:
+            final_result.append(z[i])
+        df = pd.DataFrame(final_result, index=stocks)
+        return df
     return Solve
 
-def DataFrame(f):
-    def Handle(*a, **b):
-        Y = f(*a, **b)
-        data = []
-        indexs = []
-        for i in Y:
-            indexs.append(i)
-            data.append(Y[i])
-        return pd.DataFrame(data, index=indexs)
-    return Handle
+@CreateDataFrame
+@GatherSentiment(-0.45, 0.45, 0.2, 0.8)
+@ExtractFromHTML
+def FetchData(pages, size):
+    def CutTicks(x):
+        if ':' in x:
+            x = x.split(':')[1]
+            return x
+        return x
+    result = []
+    stocks = []
+    for page in range(pages):
+        print("Fetching page number ", page)
+        url = FMPNews(page=page, size=size)
+        resp = requests.get(url).json()
+        resp = resp['content']
+        stocks += [CutTicks(t['tickers']) for t in resp]
+        result += [t['content'] for t in resp]
+        time.sleep(0.7)
+    tickers = list(set(stocks))
+    return tickers, stocks, result
 
-class Data:
+pages = 1
+size = 500
 
-    def __init__(self):
-        self.session = requests.Session()
+sentiment = FetchData(pages, size)
 
-    def fetchData(self, page=0, size=3):
-        print(f"Fetching Page: {page} with a size of {size}")
-        resp = self.session.get(FMPNews(page=page, size=size)).json()
-        df = pd.DataFrame(resp['content'])
-        time.sleep(1)
-        return df
-    
-    @CleanHTML
-    @Extract
-    def extractData(self, df):
-        def parseTicks(tick):
-            try:
-                answer =tick.split(':')[1]
-            except:
-                answer = tick
-            return answer
-        tickers = list(map(parseTicks, df['tickers'].values.tolist()))
-        news = df['content'].values.tolist()
-        stocks = list(set(tickers))
-        return stocks, tickers, news
-    
-    @DataFrame
-    def classifyNewsSentiment(self, newsData):
-        scores = []
-        tickers = list(newsData.keys())
-        count = {tick:{'positive':0,'negative':0,'opinion':0,'fact':0} for tick in tickers}
-        for ticker, article in newsData.items():
-            for sentence in article:
-                analyze = TextBlob(sentence).sentiment
-                polarity = analyze.polarity
-                subjective = analyze.subjectivity
-                
-                if subjective >= 0.75:
-                    count[ticker]['fact'] += 1
-                elif subjective <= 0.25:
-                    count[ticker]['opinion'] += 1
-                else:
-                    pass 
-                
-                if polarity >= 0.4:
-                    count[ticker]['positive'] += 1
-                elif polarity <= -0.4:
-                    count[ticker]['negative'] += 1
-                else:
-                    pass
-        return count
-                
-            
+sentiment = sentiment.sort_values(by='positive', ascending=False)
 
-pages = (0, 1, 2, 3, 4)
+import matplotlib.pyplot as plt
 
-finalSet = []
+x = sentiment['opinion'].values
+y = sentiment['fact'].values
 
-client = Data()
-for page in pages:
-    df = client.fetchData(page=page, size=30)
-    newsData = client.extractData(df)
-    sentiment = client.classifyNewsSentiment(newsData)
-    if page == 0:
-        finalSet = sentiment
-    else:
-        finalSet = pd.concat([finalSet, sentiment], ignore_index=False)
-
-print(finalSet)
-
-
-
+plt.scatter(x, y, color='red')
+plt.show()
